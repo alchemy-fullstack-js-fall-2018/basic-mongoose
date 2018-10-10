cf69738ff75e67bd7af5e32ea4b070aaa96654e3
Claire Flanagan <claireflanagan@gmail.com>
2018-10-10 11:33:52 -0700

    update otter test

--- test/app.test.js
+++ test/app.test.js
@@ -75,5 +75,14 @@ describe('otter pub/sub API', () => {
                 expect(res.body).toEqual({ ...createdOtters[0], __v: expect.any(Number) });
             });
     });
+
+    it('updates an otter by id', () => {
+        return request(app)
+            .put(`/api/otters/${createdOtters[0]._id}`)
+            .send({ numOfPublishedWorks: 5 })
+            .then(res => {
+                expect(res.body).toEqual({ ...createdOtters[0], numOfPublishedWorks: 5 }); 
+            });
+    });
 });
 
